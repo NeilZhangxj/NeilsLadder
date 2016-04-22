ca14481524550c55415d23362c0034248f40819d
zhangxiaojie <neilzhangxj@me.com>
2016-04-22 10:56:43 +0800

    starup protection

--- src/ShadowSocksJob.java
+++ src/ShadowSocksJob.java
@@ -1,4 +1,5 @@
 import com.google.gson.Gson;
+import org.apache.log4j.Logger;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
@@ -17,26 +18,26 @@ import java.util.*;
  * Created by ZhangXiaoJie on 2016/4/18.
  */
 public class ShadowSocksJob implements Job {
+//    private static Logger logger = Logger.getLogger(ShadowSocksJob.class);
     private static SchedulerFactory sf = new StdSchedulerFactory();
     private static String JOB_GROUP_NAME = "group1";
     private static String TRIGGER_GROUP_NAME = "trigger1";
 
-    public static void main(String[] args) {
-        try {
-            exeJob(); //启动时执行一次
-        } catch (IOException e) {
-            e.printStackTrace();
+    public static void main(String[] args) throws Exception {
+        while(!inInternet()){
+            Thread.sleep(60000);
         }
+
+        exeJob(); //启动时执行一次
+
         SimpleDateFormat DateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         Date d = new Date();
         String startedTime = DateFormat.format(d);
         ShadowSocksJob job = new ShadowSocksJob();
-        try {
-            System.out.println(startedTime + "【系统启动】");
-            addJob("EpicJob", job, "0 1 6/6 * * ?");
-        }  catch (Exception e) {
-            e.printStackTrace();
-        }
+
+        System.out.println(startedTime + "【系统启动】");
+        addJob("EpicJob", job, "0 1 6/6 * * ?");
+
     }
 
     public static void addJob(String jobName,Job job,String time)
@@ -136,5 +137,15 @@ public class ShadowSocksJob implements Job {
         returnMap.put("availabilityStatistics", false);
 
         return returnMap;
+    }
+
+    public static boolean inInternet(){
+        try {
+            Jsoup.connect("www.baidu.com");
+            return true;
+        } catch (Exception e){
+            System.out.println("网络连接失败");
+            return false;
+        }
     }
 }
